f26e69a7fa73ce6efa6086e0f9d4351e18852f3a
Salta <s.t.janibekova@gmail.com>
2020-04-14 20:43:08 +0600

    network subnetting calculating

--- ssh_connection_and_testing_commands/ssh_connection.py
+++ ssh_connection_and_testing_commands/ssh_connection.py
@@ -4,108 +4,114 @@ import time
 import sys
 import re
 
-#Checking username/password file
-#Prompting user for input - USERNAME/PASSWORD FILE
+# Checking username/password file
+# Prompting user for input - USERNAME/PASSWORD FILE
 user_file = input("\n# Enter user file path and name (e.g. D:\MyApps\myfile.txt): ")
 
-#Verifying the validity of the USERNAME/PASSWORD file
+# Verifying the validity of the USERNAME/PASSWORD file
 if os.path.isfile(user_file) == True:
     print("\n* Username/password file is valid :)\n")
 
 else:
     print("\n* File {} does not exist :( Please check and try again.\n".format(user_file))
     sys.exit()
-        
-#Checking commands file
-#Prompting user for input - COMMANDS FILE
+
+# Checking commands file
+# Prompting user for input - COMMANDS FILE
 cmd_file = input("\n# Enter commands file path and name (e.g. D:\MyApps\myfile.txt): ")
 
-#Verifying the validity of the COMMANDS FILE
+# Verifying the validity of the COMMANDS FILE
 if os.path.isfile(cmd_file) == True:
     print("\n* Command file is valid :)\n")
 
 else:
     print("\n* File {} does not exist :( Please check and try again.\n".format(cmd_file))
     sys.exit()
-    
-#Open SSHv2 connection to the device
+
+
+# Open SSHv2 connection to the device
 def ssh_connection(ip):
-    
     global user_file
     global cmd_file
-    
-    #Creating SSH CONNECTION
+
+    # Creating SSH CONNECTION
     try:
-        #Define SSH parameters
+        # Define SSH parameters
         selected_user_file = open(user_file, 'r')
-        
-        #Starting from the beginning of the file
+
+        # Starting from the beginning of the file
         selected_user_file.seek(0)
-        
-        #Reading the username from the file
+
+        # Reading the username from the file
         username = selected_user_file.readlines()[0].split(',')[0].rstrip("\n")
-        
-        #Starting from the beginning of the file
+
+        # Starting from the beginning of the file
         selected_user_file.seek(0)
-        
-        #Reading the password from the file
+
+        # Reading the password from the file
         password = selected_user_file.readlines()[0].split(',')[1].rstrip("\n")
-        
-        #Logging into device
+
+        # Logging into device
         session = paramiko.SSHClient()
-        
-        #For testing purposes, this allows auto-accepting unknown host keys
-        #Do not use in production! The default would be RejectPolicy
+
+        # For testing purposes, this allows auto-accepting unknown host keys
+        # Do not use in production! The default would be RejectPolicy
         session.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        
-        #Connect to the device using username and password          
-        session.connect(ip.rstrip("\n"), username = username, password = password)
-        
-        #Start an interactive shell session on the router
-        connection = session.invoke_shell()	
-        
-        #Setting terminal length for entire output - disable pagination
+
+        # Connect to the device using username and password
+        session.connect(ip.rstrip("\n"), username=username, password=password)
+
+        # Start an interactive shell session on the router
+        connection = session.invoke_shell()
+
+        # Setting terminal length for entire output - disable pagination
         connection.send("enable\n")
         connection.send("terminal length 0\n")
         time.sleep(1)
-        
-        #Entering global config mode
+
+        # Entering global config mode
         connection.send("\n")
         connection.send("configure terminal\n")
         time.sleep(1)
-        
-        #Open user selected file for reading
+
+        # Open user selected file for reading
         selected_cmd_file = open(cmd_file, 'r')
-            
-        #Starting from the beginning of the file
+
+        # Starting from the beginning of the file
         selected_cmd_file.seek(0)
-        
-        #Writing each line in the file to the device
+
+        # Writing each line in the file to the device
         for each_line in selected_cmd_file.readlines():
             connection.send(each_line + '\n')
             time.sleep(2)
-        
-        #Closing the user file
+
+        # Closing the user file
         selected_user_file.close()
-        
-        #Closing the command file
+
+        # Closing the command file
         selected_cmd_file.close()
-        
-        #Checking command output for IOS syntax errors
+
+        # Checking command output for IOS syntax errors
         router_output = connection.recv(65535)
-        
+
         if re.search(b"% Invalid input", router_output):
             print("* There was at least one IOS syntax error on device {} :(".format(ip))
-            
+
         else:
             print("\nDONE for device {} :)\n".format(ip))
-            
-        #Test for reading command output
-        print(str(router_output) + "\n")
-        
-        #Closing the connection
+
+        # Test for reading command output
+        # print(str(router_output) + "\n")
+        # filter the output
+        router_output = str(router_output)
+        all_ips = re.findall('[0-9]{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', router_output)
+        loopback_address = all_ips[0]
+        print("Loopback address: ", all_ips)
+
+        # Closing the connection
         session.close()
-     
+
     except paramiko.AuthenticationException:
-        print("* Invalid username or password :( \n* Please check the username/password file or the device configuration.")
+        print(
+            "* Invalid username or password :( \n* Please check the username/password file or the device configuration.")
         print("* Closing program... Bye!")
