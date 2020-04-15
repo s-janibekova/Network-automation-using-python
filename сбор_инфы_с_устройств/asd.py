9f6eaca64399689299429280dac56c0f968dc400
Salta <s.t.janibekova@gmail.com>
2020-04-15 17:31:45 +0600

    checking cpu of devices and plot them

---
+++ сбор_инфы_с_устройств/asd.py
@@ -0,0 +1,4 @@
+with open(
+        r"C:\Users\Alser\Desktop\Рабочий стол\NLP-Project\electronics\python-network-scripts\сбор_инфы_с_устройств\file\cpu.txt",
+        'a') as f:
+    f.write('sdaasdasd' + '\n')
